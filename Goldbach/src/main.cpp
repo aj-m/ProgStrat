37464f0d6c3644f4cec1334de444aa7806f8a3f4
Andrew Montenigro <andrew.montenigro@gmail.com>
2014-09-01 12:25:36 -0500

    Updated from class
    Set t in for loop to prevent problems in debug

--- Goldbach/src/main.cpp
+++ Goldbach/src/main.cpp
@@ -3,6 +3,7 @@
 //Time start:	22:41 CST
 
 #include <iostream>
+#include <fstream>
 #include <math.h>
 using namespace std;
 
@@ -14,27 +15,38 @@ int main() {
 	int t = 0;
 	int** addLo;
 	int** addHi;
+	ifstream infile;
+	ofstream outfile;
+	infile.open("goldbach.in");
+	outfile.open("goldbach.out");
 
 	//Generate list of primes
 	//3432 prime numbers < 32000
 	//31991 is the last prime number < 32000
-	/*
+	
 	//loop used to find count of primes < 32000
+	/*
 	for(int i = 0; i < 32000; i++){
 		if(isPrime(i)) t++;
 	}
 	cout << t << endl;
 	*/
-	
-	for(int i = 0; i < 31993; i++){
+	///*
+	for(int i = 0, t = 0; i < 32000; i++){
 		if(isPrime(i)){
 			primesList[t] = i;
 			t++;
 		}
 	}
+	for (int i = 0; i < 10; i++){
+		cout << primesList[i] << endl;
+	}
+	cout << primesList[3431] << endl;
+	//*/
 	//cout << t << endl;
+	//cout << isPrime(5) << endl;
 
-	cin >> n;		//Assumes 1 <= n <= 100
+	//cin >> n;		//Assumes 1 <= n <= 100
 	
 	
 	return 0;
@@ -43,6 +55,7 @@ int main() {
 
 bool isPrime(int num){
 	//cout << "trace "<< num << endl;
+	//I got this algorithm from the Wikipedia page for finding prime numbers
 	if (num <= 3){
 		if (num <= 1) return false;
         return true;
